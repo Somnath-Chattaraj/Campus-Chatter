17388c3529b046837357814e7b417d56a0e932c6
tanish35 <tanishmajumdar2912@gmail.com>
2024-07-28 17:16:38 +0530

    Register user complete with foreign keys

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-import { Request, Response } from 'express';
-import { registerUser } from '../controllers/userControllers';
+import express from "express";
+import { Request, Response } from "express";
+import { registerUser } from "../controllers/userControllers";
 
 const router = express.Router();
 
-router.route('/register').post(registerUser);
+router.route("/register").post(registerUser);
 
 export default router;
