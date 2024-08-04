479bfbd56165e558ad33a74c08ea6b87dbd6ad2b
tanish35 <tanishmajumdar2912@gmail.com>
2024-08-04 08:33:43 +0530

    Change file structure

--- frontend/src/components/routes/mainroute.jsx
+++ frontend/src/components/routes/mainroute.jsx
@@ -1,69 +1,71 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
-import { Mainbuttons ,Createroom,Joinroom} from "../chatroomui/main";
+import { Mainbuttons, Createroom, Joinroom } from "../chatroomui/main";
 import Chatroom from "../chatroomui/chatroom";
 import { Outlet } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import { chatRoomApi } from "../contexts/chatRoomApi";
 import { useState } from "react";
-import Header from "../Header";
-import Discover from "../Discover";
-import Services from "../Services";
-import Explore from "../Explore";
-import Testimonials from "../Testimonials";
-import Footer from "../Footer";
-import "../../styles.css"
-const Test = () =>{
+import HomePage from "../../pages/HomePage";
+const Test = () => {
   const [userId, setUserId] = useState("");
-  const [roomId,setRoomId] = useState("");
+  const [roomId, setRoomId] = useState("");
   const [targetUserId, setTargetUserId] = useState("");
   return (
     <>
-    <chatRoomApi.Provider value={{userId,setUserId,targetUserId,setTargetUserId,roomId,setRoomId}}>
-    <Outlet></Outlet>
-    </chatRoomApi.Provider>
+      <chatRoomApi.Provider
+        value={{
+          userId,
+          setUserId,
+          targetUserId,
+          setTargetUserId,
+          roomId,
+          setRoomId,
+        }}
+      >
+        <Outlet></Outlet>
+      </chatRoomApi.Provider>
     </>
-  )
-}
-const Header2 = ()=>{
-  const navigate = useNavigate();
-  return(
-    <div>
-      <Header />
-      <Discover />
-      <Services />
-      <Explore />
-      <Testimonials />
-      <Footer />
-    </div>
-  )
-}
+  );
+};
+// const Header2 = ()=>{
+//   const navigate = useNavigate();
+//   return(
+//     <div>
+//       <Header />
+//       <Discover />
+//       <Services />
+//       <Explore />
+//       <Testimonials />
+//       <Footer />
+//     </div>
+//   )
+// }
 const Mainrouter = createBrowserRouter([
   {
-    path :"/",
-    element:<Header2/>,
+    path: "/",
+    element: <HomePage />,
   },
   {
     path: "/room",
-    element : <Test/>,
-    children:[
+    element: <Test />,
+    children: [
       {
         path: "/room",
-        element:  <Mainbuttons/> ,
-        index:true,
+        element: <Mainbuttons />,
+        index: true,
       },
       {
         path: "/room/createroom",
-        element:  <Createroom/> ,
+        element: <Createroom />,
       },
       {
         path: "/room/joinroom",
-        element:  <Joinroom/> ,
+        element: <Joinroom />,
       },
       {
         path: "/room/chatting",
-        element:  <Chatroom/> ,
-      }
-    ]
-  }
+        element: <Chatroom />,
+      },
+    ],
+  },
 ]);
 export default Mainrouter;
