262cf982006052159df552597a53fd2b5147a6b6
Somnath Chattaraj <somnathchattaraj5@gmail.com>
2024-10-01 15:48:05 +0530

    Update smptp server details

--- backend/src/mail/sendMail.ts
+++ backend/src/mail/sendMail.ts
@@ -1,54 +1,29 @@
-// import nodemailer from "nodemailer";
-// import asyncHandler from "express-async-handler";
-// const transporter = nodemailer.createTransport({
-//   // @ts-ignore
-//   host: process.env.BREVO_HOST,
-//   port: process.env.BREVO_PORT,
-//   secure: false, // Use `true` for port 465, `false` for all other ports
-//   auth: {
-//     user: process.env.BREVO_USER,
-//     pass: process.env.BREVO_PASSWORD,
-//   },
-// });
-
-// const sendMail = asyncHandler(async (email, url) => {
-//   await transporter.sendMail({
-//     from: '"Review" <verify@review.com>',
-//     // @ts-ignore
-//     to: email,
-//     subject: "URL/OTP for verification",
-//     text: "Single use URL/OTP",
-//     // @ts-ignore
-//     html: url,
-//   });
-// });
-
-// export default sendMail;
-
 import nodemailer from 'nodemailer';
 
-const sendMail = () => {
-  // Step 1: Create a transporter object with SMTP server details
+const sendMail = (htmlContent: any, receiverEmail: string) => {
+  const port = process.env.SMTP_PORT;
+  const host = process.env.SMTP_HOST;
+  const senderEmail = process.env.SMTP_EMAIL;
+  const password = process.env.SMTP_PASSWORD;
   let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com', // Replace with your SMTP server (e.g., smtp.gmail.com for Gmail)
-    port: 465, // Use 587 for TLS or 465 for SSL
-    secure: true, // Set to true if using port 465 (SSL)
+    // @ts-ignore
+    host: 'smtp.gmail.com',
+    port: port, 
+    secure: true, 
     auth: {
-      user: 'somnathchattaraj51@gmail.com', // Your SMTP username (email)
-      pass: 'ejqcrhjwectmivcd',    // Your SMTP password
+      user: host, 
+      pass: password,
     },
   });
 
-  // Step 2: Define the email options (sender, receiver, subject, and body)
   let mailOptions = {
-    from: '"Campus-Chatter Admin" campus_chatter@gmail.com', // Sender address
-    to: 'somnathc.it.ug@jadavpuruniversity.in',            // List of recipients
-    subject: 'Hello from Node.js!',               // Subject line
-    text: 'This is a test email sent using Nodemailer.', // Plain text body
-    html: '<b>This is a test email sent using Nodemailer.</b>', // HTML body (optional)
+    from: `"Campus-Chatter Admin" <${senderEmail}>`, 
+    to: receiverEmail,            
+    subject: 'OTP Verification',               
+    text: htmlContent, 
+    html: htmlContent, 
   };
 
-  // Step 3: Send the email
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       return console.log('Error while sending email:', error);
