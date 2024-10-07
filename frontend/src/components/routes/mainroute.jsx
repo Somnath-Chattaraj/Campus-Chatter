e6cc7fac02b3170575ecb625515f4195d8aafbc3
tanish35 <tanishmajumdar2912@gmail.com>
2024-10-07 15:00:52 +0530

    Add addDetails page,Check if google/github signin is by student account

--- frontend/src/components/routes/mainroute.jsx
+++ frontend/src/components/routes/mainroute.jsx
@@ -1,7 +1,6 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { Mainbuttons, Createroom, Joinroom } from "../chatroomui/main";
 import Chatroom from "../chatroomui/chatroom";
-
 import { Outlet } from "react-router-dom";
 import { chatRoomApi } from "../contexts/chatRoomApi";
 import { useState } from "react";
@@ -13,44 +12,31 @@ import LoginPage from "../Login";
 import AddDetails from "../AddDetails";
 import Navbar from "../MainNavbar";
 import Loader from "../loading";
+
 const Test = () => {
   const [userId, setUserId] = useState("");
   const [roomId, setRoomId] = useState("");
   const [targetUserId, setTargetUserId] = useState("");
+
   return (
-    <>
-      <chatRoomApi.Provider
-        value={{
-          userId,
-          setUserId,
-          targetUserId,
-          setTargetUserId,
-          roomId,
-          setRoomId,
-        }}
-      >
-        <Outlet></Outlet>
-      </chatRoomApi.Provider>
-    </>
+    <chatRoomApi.Provider
+      value={{
+        userId,
+        setUserId,
+        targetUserId,
+        setTargetUserId,
+        roomId,
+        setRoomId,
+      }}
+    >
+      <Outlet />
+    </chatRoomApi.Provider>
   );
 };
-// const Header2 = ()=>{
-//   const navigate = useNavigate();
-//   return(
-//     <div>
-//       <Header />
-//       <Discover />
-//       <Services />
-//       <Explore />
-//       <Testimonials />
-//       <Footer />
-//     </div>
-//   )
-// }
+
 const Mainrouter = createBrowserRouter([
   {
     path: "/",
-
     element: <HomePage />,
   },
   {
@@ -77,54 +63,50 @@ const Mainrouter = createBrowserRouter([
     path: "/room",
     element: <Test />,
     children: [
-
-    element: <Navbar />,
-    children:[
       {
-        path:"homepage",
-        element:<HomePage></HomePage>
-      },
-      {
-        path: "posts",
-        element: <Posts />,
-      },
-      {
-        path: "posts/:id",
-        element: <SinglePost />,
-      },
-      {
-        path: "login",
-        element: <LoginPage />,
-      },
-      {
-        path: "/signup",
-        element: <Register />,
-      },
-      {
-        path: "/room",
-        element: <Test />,
+        element: <Navbar />,
         children: [
           {
-            path: "/room",
+            path: "homepage",
+            element: <HomePage />,
+          },
+          {
+            path: "posts",
+            element: <Posts />,
+          },
+          {
+            path: "posts/:id",
+            element: <SinglePost />,
+          },
+          {
+            path: "login",
+            element: <LoginPage />,
+          },
+          {
+            path: "signup",
+            element: <Register />,
+          },
+          {
+            path: "room",
             element: <Mainbuttons />,
             index: true,
           },
           {
-            path: "/room/createroom",
+            path: "room/createroom",
             element: <Createroom />,
           },
           {
-            path: "/room/joinroom",
+            path: "room/joinroom",
             element: <Joinroom />,
           },
           {
-            path: "/room/chatting",
+            path: "room/chatting",
             element: <Chatroom />,
           },
         ],
       },
-
     ],
   },
 ]);
+
 export default Mainrouter;
