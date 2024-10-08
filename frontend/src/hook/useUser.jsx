b35dd0ea7272c807407555bb408ee8713ec39ca7
Somnath Chattaraj <somnathchattaraj5@gmail.com>
2024-10-08 16:39:40 +0530

    Added useUser Hook

--- frontend/src/hook/useUser.jsx
+++ frontend/src/hook/useUser.jsx
@@ -7,7 +7,7 @@ export const useUser = () => {
 
     async function getDetails() {
         try {
-            const res = await axios.get('/', {
+            const res = await axios.get('/user/me', {
                 withCredentials: true,
             });
             setUserDetails(res.data);
