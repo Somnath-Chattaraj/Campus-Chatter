85dab14e08924eef0c2a962870258b2c355cfdc4
tanish35 <tanishmajumdar2912@gmail.com>
2024-10-10 14:23:26 +0530

    Minor change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
+import dotenv from "dotenv";
+dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_BACKEND_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // Optional: remove '/api' from the path
+      },
     },
   },
 });
