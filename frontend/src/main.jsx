6139d9fad9ff949391f2d6bcd577fa2844a7a8e1
tanish35 <tanishmajumdar2912@gmail.com>
2024-10-10 14:20:09 +0530

    Minor change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
