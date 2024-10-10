8d0f834aec6d78e5a6b2a9aa6ab9e1e3562b2282
Somnath Chattaraj <somnathchattaraj5@gmail.com>
2024-10-10 13:30:50 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://chatterapi.campusify.site";
+export const BACKEND_URL = "https://chatterapi.campusify.site/api";
 export const WEBSOCKET_URL = "wss://realtime.campusify.site";
